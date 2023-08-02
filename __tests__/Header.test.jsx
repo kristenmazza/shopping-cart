7a9581ef8f07b4ef69f98d436e25089a14144e30
kristenmazza <kbmazza@gmail.com>
2023-08-02 12:47:45 -0700

    Test Navbar component

--- __tests__/Header.test.jsx
+++ __tests__/Header.test.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Header from "../src/components/header";
 import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from "react-router-dom";
 
 describe("Header component", () => {
     it("renders correct heading", () => {
-        render(<Header />);
+        render(<MemoryRouter><Header /></MemoryRouter>);
         expect(screen.getByRole("heading").textContent).toMatch(/lumina/i);
     });
 });
--- __tests__/Navbar.test.jsx
+++ __tests__/Navbar.test.jsx
@@ -1,13 +1,25 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import Navbar from "../src/components/Navbar";
-// import { describe, it, expect } from 'vitest';
-// import TestRenderer from 'react-test-renderer';
-// import { MemoryRouter } from "react-router-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Navbar from "../src/components/Navbar";
+import { describe, it, expect } from 'vitest';
+import TestRenderer from 'react-test-renderer';
+import { MemoryRouter } from "react-router-dom";
 
-// describe("Navbar component", () => {
-//     it("test the children inside Navbar component", () => {
-//         const navbar = TestRenderer.create(<MemoryRouter><Navbar /></MemoryRouter>).toJSON();
-//         console.log(navbar);
-//     });
-// });
+describe("Navbar component", () => {
+    it("test the children inside Navbar component", () => {
+        const navbar = TestRenderer.create(<MemoryRouter><Navbar /></MemoryRouter>).toJSON();
+
+        // Shop route
+        expect(navbar.children[1].children[0].children[0].children[0].type).toBe('a');
+        expect(navbar.children[1].children[0].children[0].children[0].props.href).toBe('/shop');
+
+        // About route
+        expect(navbar.children[1].children[0].children[1].children[0].type).toBe('a');
+        expect(navbar.children[1].children[0].children[1].children[0].props.href).toBe('/about');
+    });
+
+    it("will create a snapshot", () => {
+        const navbar = TestRenderer.create(<MemoryRouter><Navbar /></MemoryRouter>).toJSON();
+        expect(navbar).toMatchSnapshot();
+    })
+});
