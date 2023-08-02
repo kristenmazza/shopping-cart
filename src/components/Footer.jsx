60f7438af63d292924a850b6cb9b08c1a3b53ea6
kristenmazza <kbmazza@gmail.com>
2023-08-02 15:40:48 -0700

    Add footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
     return (
-        <footer className="footer">footer
+        <footer className={styles.footer}>
+            <div className={styles.createdBy}>
+                <p>created by kristenmazza</p>
+                <a href="https://github.com/kristenmazza"><img className="icon" alt="github icon" src='/images/github-icon.png' /> </a>
+            </div>
         </footer>
     )
 }
